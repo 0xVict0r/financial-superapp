eed8fd354b33dc0691e119ad48920dfc5fab32f0
0xVict0r <74269639+0xVict0r@users.noreply.github.com>
2022-05-29 10:50:57 +0200

    optimized with yfinance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import run_functions
+import yfinance as yf
 
 # Variables
 years = 20
@@ -8,7 +9,9 @@ tickers = ['AAPL', 'TSLA', 'BTC-USD']
 price_init = 1000
 
 if __name__ == "__main__":
-    # run_functions.best_portfolio_performance_estimator(tickers, price_init, years, plotting=False, num_ports=10, monte_carlo_trials=monte_carlo_trials)
-    run_functions.portfolio_performance_estimator(
-        years, plotting=False, monte_carlo_trials=monte_carlo_trials)
-    # run_functions.asset_price_estimator(ticker, years, plotting=False, monte_carlo_trials=monte_carlo_trials)
+    # yf.pdr_override()
+    run_functions.best_portfolio_performance_estimator(
+        tickers, price_init, years, mc_plotting=False, num_ports=100, monte_carlo_trials=monte_carlo_trials)
+    # run_functions.portfolio_performance_estimator(
+    #     years, mc_plotting=False, monte_carlo_trials=monte_carlo_trials)
+    # run_functions.asset_price_estimator(ticker, years, mc_plotting=False, monte_carlo_trials=monte_carlo_trials)
--- run_functions.py
+++ run_functions.py
@@ -1,32 +1,54 @@
 from email.mime import base
 import portfolio_functions
 import base_functions
+import time
 
-def best_portfolio_performance_estimator(tickers, price_init, years, plotting = False, num_ports=10, monte_carlo_trials=100000):
+
+def best_portfolio_performance_estimator(tickers, price_init, years, mc_plotting=False, num_ports=10, monte_carlo_trials=100000):
+    start = time.time()
     portfolio_data = portfolio_functions.get_portfolio_data(tickers)
-    best_sharpe, best_weights = portfolio_functions.get_best_portfolio(portfolio_data, num_ports)
-    mean, vol = portfolio_functions.get_portfolio_hist_perf(portfolio_data, best_weights)
-    price_est = base_functions.simulate_mc(price_init, vol, mean, years*252 + 1, monte_carlo_trials, 'Portfolio')
+    best_sharpe, best_weights = portfolio_functions.get_best_portfolio(
+        portfolio_data, num_ports)
+    mean, vol = portfolio_functions.get_portfolio_hist_perf(
+        portfolio_data, best_weights)
+    price_est = base_functions.simulate_mc(
+        price_init, vol, mean, years*252 + 1, monte_carlo_trials, 'Portfolio')
     portfolio_functions.print_portfolio_weights(tickers, best_weights)
     percentile_prices = base_functions.get_percentile_prices(price_est)
-    base_functions.plot_percentiles(price_init, percentile_prices, years*252 + 1)
-    if plotting:
+    base_functions.plot_percentiles(
+        price_init, percentile_prices, years*252 + 1)
+    end = time.time()
+    print("\n" + str(end - start) + "s")
+    if mc_plotting:
         base_functions.plot_all_prices(price_est)
-        
-def portfolio_performance_estimator(years, plotting = False, monte_carlo_trials=100000):
+
+
+def portfolio_performance_estimator(years, mc_plotting=False, monte_carlo_trials=100000):
+    start = time.time()
     tickers, weights, init_value = base_functions.get_weights_and_tickers()
     portfolio_data = portfolio_functions.get_portfolio_data(tickers)
-    mean, vol = portfolio_functions.get_portfolio_hist_perf(portfolio_data, weights)
-    price_est = base_functions.simulate_mc(init_value, vol, mean, years*252 + 1, monte_carlo_trials, 'Portfolio')
+    mean, vol = portfolio_functions.get_portfolio_hist_perf(
+        portfolio_data, weights)
+    price_est = base_functions.simulate_mc(
+        init_value, vol, mean, years*252 + 1, monte_carlo_trials, 'Portfolio')
     portfolio_functions.print_portfolio_weights(tickers, weights)
     percentile_prices = base_functions.get_percentile_prices(price_est)
-    base_functions.plot_percentiles(init_value, percentile_prices, years*252 + 1)
-    if plotting:
+    base_functions.plot_percentiles(
+        init_value, percentile_prices, years*252 + 1)
+    end = time.time()
+    print("\n" + str(end - start) + "s")
+    if mc_plotting:
         base_functions.plot_all_prices(price_est)
-        
-def asset_price_estimator(ticker, years, plotting = False, monte_carlo_trials=100000):
+
+
+def asset_price_estimator(ticker, years, mc_plotting=False, monte_carlo_trials=100000):
+    start = time.time()
     mean, vol, init_price = base_functions.get_asset_hist_perf(ticker)
-    price_est = base_functions.simulate_mc(init_price, vol, mean, years*252, monte_carlo_trials, ticker)
-    base_functions.plot_percentiles(init_price, base_functions.get_percentile_prices(price_est), years*252)
-    if plotting:
+    price_est = base_functions.simulate_mc(
+        init_price, vol, mean, years*252, monte_carlo_trials, ticker)
+    base_functions.plot_percentiles(
+        init_price, base_functions.get_percentile_prices(price_est), years*252)
+    end = time.time()
+    print("\n" + str(end - start) + "s")
+    if mc_plotting:
         base_functions.plot_all_prices(price_est)
